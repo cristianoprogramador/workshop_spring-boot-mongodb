6b009c7a19d3dd118e8e66e885f3d724342ceaf8
Cristiano Silva <cristiano_own@hotmail.com.br>
2022-06-09 15:08:56 -0300

    Obtendo um post por id

---
+++ src/main/java/com/cristianosilva/workshopmongo/services/PostService.java
@@ -0,0 +1,23 @@
+package com.cristianosilva.workshopmongo.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.cristianosilva.workshopmongo.domain.Post;
+import com.cristianosilva.workshopmongo.repository.PostRepository;
+import com.cristianosilva.workshopmongo.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+
+	@Autowired
+	private PostRepository repo;
+
+	public Post findById(String id) {
+		Optional<Post> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+	
+}
